3d455426b9a6c9b1d0b7af98e53a95d0bf6356af
Alex Tsarkov <alex.tsarkov@gmail.com>
2020-09-03 21:15:24 +0300

    Move io functions to utils

--- src/Command/FetchCommand.php
+++ src/Command/FetchCommand.php
@@ -2,8 +2,9 @@
 
 namespace Eternium\Command;
 
-use Eternium\Event\EventInterface;
+use Eternium\Event\Event;
 use Eternium\Event\Leaderboard;
+use Eternium\Utils;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -12,8 +13,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\HttpClient\HttpClient;
-use function Symfony\Component\String\u;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FetchCommand extends Command
@@ -23,21 +22,14 @@ class FetchCommand extends Command
     private HttpClientInterface $client;
 
     /**
-     * @var array<int, EventInterface>
+     * @var array<int, Event>
      */
     private array $events;
 
-    public function __construct(EventInterface ...$events)
+    public function __construct(Event ...$events)
     {
         $this->events = $events;
-        $this->client = HttpClient::createForBaseUri('https://mfp.makingfun.com/api/', [
-            'http_version' => '1.1',
-            'max_redirects' => 0,
-            'headers' => [
-                'Accept' => 'application/json',
-                'X-API-Key' => (string) getenv('ETERNIUM_API_KEY'),
-            ],
-        ]);
+        $this->client = Utils::createHttpClient((string) getenv('ETERNIUM_API_KEY'));
 
         parent::__construct();
     }
@@ -61,110 +53,57 @@ class FetchCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $filter = $this->filter($input->getArgument('prefix'));
-        $dir = $input->getOption('data-dir');
+        $prefix = $input->getArgument('prefix');
+        $path = $input->getOption('data-dir');
         $update = $input->getOption('update');
 
-        $fetcher = function (Leaderboard $leaderboard, string $prefix) use ($output, $filter, $dir, $update): void {
+        $fetcher = function (Leaderboard $leaderboard, string ...$names) use ($prefix, $path, $update, $output): array {
             $formatter = $this->getHelper('formatter');
 
-            if (!$filter($prefix)) {
+            $name = join('.', $names);
+            if (!str_starts_with($name, $prefix)) {
                 $output->writeln(
-                    $formatter->formatSection('SKIP', "{$prefix} not matched prefix", 'comment'),
+                    $formatter->formatSection('SKIP', "{$name} not matched prefix", 'comment'),
                     Output::VERBOSITY_VERBOSE
                 );
 
-                return;
+                return [];
             }
 
-            $file = $dir.DIRECTORY_SEPARATOR."{$prefix}.csv";
+            $file = $path.DIRECTORY_SEPARATOR."{$name}.csv";
             if (!$update && is_file($file)) {
                 $output->writeln(
-                    $formatter->formatSection('SKIP', "{$prefix} entries already dumped", 'comment'),
+                    $formatter->formatSection('SKIP', "{$name} entries already dumped", 'comment'),
                     Output::VERBOSITY_VERBOSE
                 );
 
-                return;
+                return [];
             }
 
-            $output->writeln(
-                $formatter->formatSection('DUMP', "fetching {$prefix} entries...")
-            );
+            $output->writeln($formatter->formatSection('DUMP', "fetching {$name} entries..."));
 
-            $progressBar = new ProgressBar($output);
-            $count = $this->dump($file, $progressBar->iterate($this->fetch($leaderboard->id())));
-            $progressBar->clear();
+            $reader = Utils::createLeaderboardReader($this->client, $leaderboard->getId());
+            $writer = Utils::createCsvWriter($file);
 
-            $output->writeln(
-                $formatter->formatSection('DUMP', "{$count} entries dumped")
-            );
+            try {
+                $progressBar = new ProgressBar($output);
+                foreach ($progressBar->iterate($reader) as $entry) {
+                    $writer->send($entry);
+                }
+            } finally {
+                $progressBar->clear();
+            }
+            $writer->send(null);
+
+            $output->writeln($formatter->formatSection('DUMP', "{$writer->getReturn()} entries dumped"));
+
+            return [];
         };
 
         foreach ($this->events as $event) {
-            $event->fetch($fetcher);
+            $event->apply($fetcher);
         }
 
         return self::SUCCESS;
-    }
-
-    private function filter(string $prefix): \Closure
-    {
-        if ('' === $prefix) {
-            return static fn (): bool => true;
-        }
-
-        return static fn (string $name): bool => u($name)->startsWith($prefix);
-    }
-
-    private function dump(string $file, iterable $data): int
-    {
-        $count = 0;
-
-        $memory = fopen('php://memory', 'r+');
-        foreach ($data as $entry) {
-            fputcsv($memory, $entry);
-            ++$count;
-        }
-        rewind($memory);
-
-        if (false === @file_put_contents($file, $memory, LOCK_EX)) {
-            $error = error_get_last();
-            error_clear_last();
-
-            throw new \ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line']);
-        }
-
-        return $count;
-    }
-
-    /**
-     * @return iterable<int, array>
-     */
-    private function fetch(string $id): iterable
-    {
-        $query = [
-            'page' => 0,
-            'pageSize' => 1000,
-            'payload' => 'name,champion_level,hero.selectedPlayerNameID,trialStats.heroDeaths',
-        ];
-
-        do {
-            $count = 0;
-            ++$query['page'];
-            $response = $this->client->request('GET', "leaderboards/{$id}/rankings", ['query' => $query]);
-            foreach ($response->toArray() as $data) {
-                yield [
-                    'name' => $data['payload']['name'],
-                    'title' => u($data['payload']['hero']['selectedPlayerNameID'] ?? '')
-                        ->replace('_', ' ')
-                        ->title(true)
-                        ->toString(),
-                    'level' => $data['payload']['champion_level'],
-                    'score' => $data['score'],
-                    'deaths' => $data['payload']['trialStats']['heroDeaths'],
-                ];
-                ++$count;
-            }
-        } while ($count === $query['pageSize']);
     }
 }
