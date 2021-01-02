96d01fd196ba9a6e965ad3fbbcfbc07985d804f0
Alex Tsarkov <alex.tsarkov@gmail.com>
2021-01-02 14:36:19 +0300

    Add URL utility

---
+++ src/Utils/Url.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eternium\Utils;
+
+final class Url implements \Stringable
+{
+    public function __construct(
+        public string $scheme = '',
+        public string $authority = '',
+        public string $path = '',
+        public string $query = '',
+        public string $fragment = '',
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        $url = $this->path;
+        if ('' !== $this->authority) {
+            $url = "//{$this->authority}{$url}";
+            if ('' !== $this->scheme) {
+                $url = "{$this->scheme}:{$url}";
+            }
+        }
+        if ('' !== $this->query) {
+            $url .= "?{$this->query}";
+        }
+        if ('' !== $this->fragment) {
+            $url .= "#{$this->fragment}";
+        }
+
+        return $url;
+    }
+
+    public static function parse(string $url): self
+    {
+        $parts = parse_url($url);
+        if (false === $parts) {
+            throw new \InvalidArgumentException("Malformed URL: '{$url}'");
+        }
+
+        $authority = $parts['host'] ?? '';
+        if ('' !== $authority) {
+            if (isset($parts['port'])) {
+                $authority .= ":{$parts['port']}";
+            }
+            $user = $parts['user'] ?? '';
+            if ('' !== $user) {
+                $authority = "@{$authority}";
+                $pass = $parts['pass'] ?? '';
+                if ('' !== $pass) {
+                    $authority = ":{$authority}";
+                }
+                $authority = "{$user}{$authority}";
+            }
+        }
+
+        return new self(
+            scheme: $parts['scheme'] ?? '',
+            authority: $authority,
+            path: $parts['path'] ?? '',
+            query: $parts['query'] ?? '',
+            fragment: $parts['fragment'] ?? '',
+        );
+    }
+
+    public function resolve(string $path): self
+    {
+        $url = new self(
+            scheme: $this->scheme,
+            authority: $this->authority,
+            path: $path,
+        );
+
+        if (!str_starts_with($path, '/')) {
+            $url->path = rtrim($this->path, '/')."/{$path}";
+        }
+
+        return $url;
+    }
+}
