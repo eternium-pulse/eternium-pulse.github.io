b3ae77799f3faca5bf4d94d5d71c33fb2633f059
Alex Tsarkov <alex.tsarkov@gmail.com>
2021-10-06 22:31:14 +0300

    Add Config class

--- bin/lb.php
+++ bin/lb.php
@@ -5,22 +5,16 @@ namespace Eternium\Command;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+use Eternium\Config;
 use Symfony\Component\Console\Application;
 
-define('ETERNIUM_CONFIG_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR);
+$config = new Config(dirname(__DIR__).'/config');
+
 define('ETERNIUM_DATA_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR);
 define('ETERNIUM_HTML_PATH', dirname(__DIR__).DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR);
 
-$events = require ETERNIUM_CONFIG_PATH.'events.php';
-
 $application = new Application('Eternium Pulse');
-$application->add(new FetchCommand(
-    require ETERNIUM_CONFIG_PATH.'eternium.php',
-    $events,
-));
-$application->add(new GenerateCommand(
-    require ETERNIUM_CONFIG_PATH.'twig.php',
-    $events,
-));
+$application->add(new FetchCommand($config->eternium, $config->events));
+$application->add(new GenerateCommand($config->twig, $config->events));
 
 exit($application->run());
---
+++ src/Config.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Eternium;
+
+class Config
+{
+    public function __construct(private string $path)
+    {
+        assert(is_dir($path));
+
+        $this->path = realpath($path);
+    }
+
+    public function __get(string $name)
+    {
+        assert('' !== $name);
+
+        $this->{$name} = null;
+        $file = "{$this->path}/{$name}.php";
+        if (is_file($file)) {
+            $this->{$name} = (function () {
+                return require func_get_arg(0);
+            })($file);
+        }
+
+        return $this->{$name};
+    }
+}
