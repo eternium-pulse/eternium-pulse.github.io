1dd3899a6c240054d09d4e8f413b715e4b2833ff
Alex Tsarkov <alex.tsarkov@gmail.com>
2022-03-26 15:03:51 +0300

    Minify js

--- public/main.js
+++ public/main.js
@@ -1,10 +1,30 @@
-!function(){
-"use strict";
-const s=document.getElementById('status');
-if(s){
-fetch('https://eternium.alex-tsarkov.workers.dev/api/v2/getSystemStatus')
-.then(r=>{if(!r.ok){throw new Error()}return r.json()})
-.then(({status})=>{s.classList.replace('alert-secondary','alert-'+(status.code?'danger':'success'));s.textContent=`Game version: ${status.version}. ${status.message}`})
-.catch(()=>{})
-}
-}()
+!function () {
+    "use strict";
+
+    const eternium = {
+        async systemStatus() {
+            return this.fetch('v2/getSystemStatus');
+        },
+
+        async fetch(method) {
+            return fetch(`https://eternium.alex-tsarkov.workers.dev/api/${method}`)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    throw new Error(`Method ${method} got HTTP ${response.status}`);
+                });
+        },
+    };
+
+    const status = document.getElementById('status');
+    if (status) {
+        status.classList.remove('d-none')
+        eternium.systemStatus()
+            .then(({ status }) => {
+                status.classList.replace('alert-secondary', 'alert-' + (status.code ? 'danger' : 'success'));
+                status.textContent = `Game version: ${status.version}. ${status.message}`;
+            })
+            .catch(() => { });
+    }
+}();
