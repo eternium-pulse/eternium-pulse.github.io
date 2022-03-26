77eba20410b24ef31b333698805ed937afc0abf5
Alex Tsarkov <alex.tsarkov@gmail.com>
2022-03-26 14:31:39 +0300

    Add minify_* filter

--- config/twig.php
+++ config/twig.php
@@ -1,8 +1,17 @@
 <?php
 
+use Eternium\Utils\Minifier;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Twig\TwigFilter;
 
-return new Environment(
-    new FilesystemLoader(['templates', 'public'], dirname(__DIR__)),
-);
+return (static function (): Environment {
+    $twig = new Environment(
+        new FilesystemLoader(['templates', 'public'], dirname(__DIR__)),
+    );
+    $twig->addFilter(new TwigFilter('minify_*', new Minifier(), [
+        'is_safe_callback' => static fn (string $type): ?array => [$type],
+    ]));
+
+    return $twig;
+})();
