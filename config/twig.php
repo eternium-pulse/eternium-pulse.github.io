88951116b875e7819c602fcc0cfd887e9c583740
Alex Tsarkov <alex.tsarkov@gmail.com>
2022-03-27 12:51:29 +0300

    Configure date

--- config/twig.php
+++ config/twig.php
@@ -2,6 +2,7 @@
 
 use Eternium\Utils\Minifier;
 use Twig\Environment;
+use Twig\Extension\CoreExtension;
 use Twig\Loader\FilesystemLoader;
 use Twig\TwigFilter;
 
@@ -9,9 +10,15 @@ return (static function (): Environment {
     $twig = new Environment(
         new FilesystemLoader(['templates', 'public'], dirname(__DIR__)),
     );
+
     $twig->addFilter(new TwigFilter('minify_*', new Minifier(), [
         'is_safe_callback' => static fn (string $type): ?array => [$type],
     ]));
+
+    $twig->addGlobal('now', new \DateTimeImmutable('now', new \DateTimeZone('UTC')));
+    $core = $twig->getExtension(CoreExtension::class);
+    $core->setTimezone('UTC');
+    $core->setDateFormat(\DateTime::W3C, '%d days');
 
     return $twig;
 })();
