9508687abf3a45964c49bf81522ee05a4968c45f
Alex Tsarkov <alex.tsarkov@gmail.com>
2022-03-27 20:58:48 +0300

    Add new methods

--- public/js/eternium.js
+++ public/js/eternium.js
@@ -1,13 +1,45 @@
 const eternium = {
+    async gameEvents() {
+        return this._fetchV3('getGameEvents').then(({ events }) => events);
+    },
+
+    async leaderboard(id) {
+        return this._fetchMfp(`leaderboards/${id}`);
+    },
+
+    async leaderboards() {
+        return this._fetchMfp('leaderboards');
+    },
+
+    async news() {
+        return this._fetchV2('getNews').then(({ news }) => news);
+    },
+
+    async ranking(id, ranking) {
+        return this._fetchMfp(`leaderboards/${id}/rankings/${ranking}`);
+    },
+
+    async rankings(id, { page = 1, pageSize = 25 }) {
+        return this._fetchMfp(`leaderboards/${id}/rankings?${new URLSearchParams({ page, pageSize })}`);
+    },
+
     async systemStatus() {
         return this._fetchV2('getSystemStatus').then(({ status }) => status);
     },
 
-    async _fetchV2(method) {
-        return fetch(`https://eternium.alex-tsarkov.workers.dev/api/v2/${method}`).then(this._handle);
+    async _fetchMfp(url) {
+        return this._fetch(url, 'https://eternium-mfp.alex-tsarkov.workers.dev/api/');
     },
 
-    async _handle(response) {
-        return response.ok ? response.json() : Promise.reject(response.status);
+    async _fetchV2(url) {
+        return this._fetch(url, 'https://eternium.alex-tsarkov.workers.dev/api/v2/');
+    },
+
+    async _fetchV3(url) {
+        return this._fetch(url, 'https://eternium.alex-tsarkov.workers.dev/api/v3/');
+    },
+
+    async _fetch(url, base) {
+        return fetch(new URL(url, base).href).then(async response => response.ok ? response.json() : Promise.reject(response.status));
     },
 };
