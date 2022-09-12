5f80c8ea1267a9f3b94502073a3d4eb2804cee3a
Alex Tsarkov <alex.tsarkov@gmail.com>
2022-09-12 19:30:48 +0300

    Use wikimedia/minify for assets

--- src/Utils/Minifier.php
+++ src/Utils/Minifier.php
@@ -2,18 +2,11 @@
 
 namespace Eternium\Utils;
 
-use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Wikimedia\Minify\CSSMin;
+use Wikimedia\Minify\JavaScriptMinifier;
 
 final class Minifier
 {
-    private HttpClientInterface $client;
-
-    public function __construct(HttpClientInterface $client = null)
-    {
-        $this->client = $client ?? HttpClient::createForBaseUri('https://www.toptal.com/developers/');
-    }
-
     public function __invoke(string $type, string $code): string
     {
         return $this->tryMinify($type, $code);
@@ -24,26 +17,18 @@ final class Minifier
      */
     public function minify(string $type, string $code): string
     {
-        $uri = match ($type) {
-            'css' => 'cssminifier/raw',
-            'js' => 'javascript-minifier/raw',
+        return match ($type) {
+            'css' => CssMin::minify($code),
+            'js' => JavaScriptMinifier::minify($code),
             default => throw new \DomainException('Unsupported minification type'),
         };
-
-        $options = [
-            'body' => [
-                'input' => $code,
-            ],
-        ];
-
-        return $this->client->request('POST', $uri, $options)->getContent();
     }
 
     public function tryMinify(string $type, string $code): string
     {
         try {
             return $this->minify($type, $code);
-        } catch (\DomainException) {
+        } catch (\Throwable) {
             return $code;
         }
     }
