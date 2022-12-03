952f6bb0cc61a3fd4e17167af9fbb7058e03ccfb
Alex Tsarkov <alex.tsarkov@gmail.com>
2022-12-03 18:26:04 +0300

    Use placeholder

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,14 @@ Promise.all([
     new Promise((resolve, reject) => {
         const elem = document.getElementById('status');
         if (elem) {
-            elem.classList.remove('d-none');
-            resolve(elem);
+            elem.classList.replace('d-none', 'd-block');
+            resolve(elem.querySelector('.placeholder'));
         } else {
             reject();
         }
     }),
     eternium.systemStatus(),
 ]).then(([elem, { version, code, message }]) => {
-    elem.classList.replace('alert-secondary', 'alert-' + (code ? 'danger' : 'success'));
+    elem.classList.replace('placeholder', 'alert-' + (code ? 'danger' : 'success'));
     elem.textContent = `Game version: ${version}. ${message}`;
 });
