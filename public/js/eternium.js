258b1a9c653903d7061f6947deb0e500f689c42f
Alex Tsarkov <alex.tsarkov@gmail.com>
2023-02-28 18:08:29 +0300

    Set new API endpoint

--- public/js/eternium.js
+++ public/js/eternium.js
@@ -1,41 +1,29 @@
 const eternium = new class Eternium {
     async gameEvents() {
-        return this._fetchMM('getGameEvents').then(({ events }) => events);
+        return this._fetch('events');
     }
 
     async leaderboard(id) {
-        return this._fetchMFP(`leaderboards/${id}`);
+        return this._fetch(`leaderboards/${id}`);
     }
 
     async leaderboards() {
-        return this._fetchMFP('leaderboards');
+        return this._fetch('leaderboards');
     }
 
     async ranking(id, ranking) {
-        return this._fetchMFP(`leaderboards/${id}/rankings/${ranking}`);
+        return this._fetch(`leaderboards/${id}/rankings/${ranking}`);
     }
 
     async rankings(id, { page = 1, pageSize = 25 }) {
-        return this._fetchMFP(`leaderboards/${id}/rankings?${new URLSearchParams({ page, pageSize })}`);
+        return this._fetch(`leaderboards/${id}/rankings?${new URLSearchParams({ page, pageSize })}`);
     }
 
     async systemStatus() {
-        return this._fetchMM('getSystemStatus').then(({ status }) => status);
+        return this._fetch('status');
     }
 
-    async systemStatusList() {
-        return this._fetchMM('getSystemStatusList').then(({ status }) => status);
-    }
-
-    async _fetch(url, base) {
-        return fetch(new URL(url, base).href).then(async response => response.ok ? response.json() : Promise.reject(response.status));
-    }
-
-    async _fetchMFP(url) {
-        return this._fetch(url, 'https://eternium-mfp.alex-tsarkov.workers.dev/api/');
-    }
-
-    async _fetchMM(url) {
-        return this._fetch(url, 'https://eternium.alex-tsarkov.workers.dev/api/v2/');
+    async _fetch(url) {
+        return fetch(new URL(url, 'https://eternium.pages.dev/api/v1/').href).then(async response => response.ok ? response.json() : Promise.reject(response.status));
     }
 };
