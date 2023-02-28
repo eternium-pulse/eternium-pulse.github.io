b0b323f1775b0b8638188b3f25f60175db05869a
Alex Tsarkov <alex.tsarkov@gmail.com>
2023-02-28 11:12:20 +0300

    Refactor eternium js

--- public/js/eternium.js
+++ public/js/eternium.js
@@ -1,45 +1,41 @@
-const eternium = {
+const eternium = new class Eternium {
     async gameEvents() {
-        return this._fetchV3('getGameEvents').then(({ events }) => events);
-    },
+        return this._fetchMM('getGameEvents').then(({ events }) => events);
+    }
 
     async leaderboard(id) {
-        return this._fetchMfp(`leaderboards/${id}`);
-    },
+        return this._fetchMFP(`leaderboards/${id}`);
+    }
 
     async leaderboards() {
-        return this._fetchMfp('leaderboards');
-    },
-
-    async news() {
-        return this._fetchV2('getNews').then(({ news }) => news);
-    },
+        return this._fetchMFP('leaderboards');
+    }
 
     async ranking(id, ranking) {
-        return this._fetchMfp(`leaderboards/${id}/rankings/${ranking}`);
-    },
+        return this._fetchMFP(`leaderboards/${id}/rankings/${ranking}`);
+    }
 
     async rankings(id, { page = 1, pageSize = 25 }) {
-        return this._fetchMfp(`leaderboards/${id}/rankings?${new URLSearchParams({ page, pageSize })}`);
-    },
+        return this._fetchMFP(`leaderboards/${id}/rankings?${new URLSearchParams({ page, pageSize })}`);
+    }
 
     async systemStatus() {
-        return this._fetchV2('getSystemStatus').then(({ status }) => status);
-    },
+        return this._fetchMM('getSystemStatus').then(({ status }) => status);
+    }
 
-    async _fetchMfp(url) {
-        return this._fetch(url, 'https://eternium-mfp.alex-tsarkov.workers.dev/api/');
-    },
-
-    async _fetchV2(url) {
-        return this._fetch(url, 'https://eternium.alex-tsarkov.workers.dev/api/v2/');
-    },
-
-    async _fetchV3(url) {
-        return this._fetch(url, 'https://eternium.alex-tsarkov.workers.dev/api/v3/');
-    },
+    async systemStatusList() {
+        return this._fetchMM('getSystemStatusList').then(({ status }) => status);
+    }
 
     async _fetch(url, base) {
         return fetch(new URL(url, base).href).then(async response => response.ok ? response.json() : Promise.reject(response.status));
-    },
+    }
+
+    async _fetchMFP(url) {
+        return this._fetch(url, 'https://eternium-mfp.alex-tsarkov.workers.dev/api/');
+    }
+
+    async _fetchMM(url) {
+        return this._fetch(url, 'https://eternium.alex-tsarkov.workers.dev/api/v2/');
+    }
 };
