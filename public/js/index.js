8fb55f7d2eb0a1e7c55d4c917cfb2cad06f00c6f
Alex Tsarkov <alex.tsarkov@gmail.com>
2023-03-01 11:05:05 +0300

    Render game versions

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,5 @@
-Promise.all([
-    new Promise((resolve, reject) => {
-        const elem = document.getElementById('status');
-        if (elem) {
-            elem.classList.replace('d-none', 'd-block');
-            resolve(elem.querySelector('.placeholder'));
-        } else {
-            reject();
-        }
-    }),
-    eternium.systemStatus(),
-]).then(([elem, { version, code, message }]) => {
-    elem.classList.replace('placeholder', 'alert-' + (code ? 'danger' : 'success'));
-    elem.textContent = `Game version: ${version}. ${message}`;
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('time').forEach(time => {
+        time.textContent = new Date(time.dateTime).toLocaleString();
+    })
 });
