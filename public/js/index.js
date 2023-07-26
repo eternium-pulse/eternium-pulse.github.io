52812fb65eedf7a3d5889f77cbb17b705f614722
Alex Tsarkov <alex.tsarkov@gmail.com>
2023-07-26 18:18:54 +0300

    fix(html): CSS selector typo

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('time:not([datetime=^P])').forEach(time => {
+    document.querySelectorAll('time:not([datetime^=P])').forEach(time => {
         time.textContent = new Date(time.dateTime).toLocaleString();
     })
 });
