1707de6f7083e17f2f9d63ef9f673f3a226ef380
Alex Tsarkov <alex.tsarkov@gmail.com>
2023-07-27 17:54:04 +0300

    fix(sitemap): add default priority constant

--- src/Sitemap/Url.php
+++ src/Sitemap/Url.php
@@ -8,11 +8,13 @@ use League\Uri\Contracts\UriInterface;
 
 final class Url implements \Stringable
 {
+    public const DEFAULT_PRIORITY = 0.5;
+
     public function __construct(
         public readonly UriInterface $loc,
         public readonly ?\DateTimeInterface $lastmod = null,
         public readonly ?Changefreq $changefreq = null,
-        public readonly float $priority = 0.5,
+        public readonly float $priority = self::DEFAULT_PRIORITY,
     ) {
         \assert('http' === $loc->getScheme() || 'https' === $loc->getScheme());
         \assert(0 <= $priority && $priority <= 1);
