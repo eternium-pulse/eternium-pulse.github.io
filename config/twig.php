a99af2028169c28e0b9771b319bda5de5271ff74
Alex Tsarkov <alex.tsarkov@gmail.com>
2024-01-06 19:33:08 +0300

    fix(config): configure Twig

--- config/twig.php
+++ config/twig.php
@@ -7,42 +7,44 @@ use Twig\Loader\FilesystemLoader;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
-return (static function (): Environment {
-    $twig = new Environment(
-        new FilesystemLoader(['templates', 'public'], dirname(__DIR__)),
-    );
+$twig = new Environment(
+    new FilesystemLoader(['templates', 'public'], \dirname(__DIR__)),
+);
 
-    $twig->addFilter(new TwigFilter('minify_*', new Minifier(), [
-        'is_safe_callback' => static fn (string $type): ?array => [$type],
-    ]));
+$twig->addFilter(new TwigFilter('minify_*', new Minifier(), [
+    'is_safe_callback' => static fn (string $type): ?array => [$type],
+]));
 
-    $twig->addFunction(new TwigFunction('detect_country', function (string $name): ?string {
-        if (\str_starts_with($name, 'Kor')) {
-            return 'KR';
-        }
-        if (\str_starts_with($name, 'China')) {
-            return 'CN';
-        }
-        if (\str_starts_with($name, 'Ru')) {
-            return 'RU';
-        }
+$twig->addFunction(new TwigFunction('detect_country', static function (string $name): ?string {
+    if (\str_starts_with($name, 'Kor')) {
+        return 'KR';
+    }
+    if (\str_starts_with($name, 'China')) {
+        return 'CN';
+    }
+    if (\str_starts_with($name, 'Ru')) {
+        return 'RU';
+    }
 
-        return null;
-    }));
+    return null;
+}));
 
-    $twig->addFunction(new TwigFunction('env', function (string $var, string $default = ''): string {
-        $value = \getenv($var);
-        if (false === $value) {
-            $value = $default;
-        }
+$twig->addFunction(new TwigFunction('env', static function (string $var, string $default = ''): string {
+    $value = \getenv($var);
+    if (false === $value) {
+        $value = $default;
+    }
 
-        return $value;
-    }));
+    return $value;
+}));
 
-    $twig->addGlobal('now', new \DateTimeImmutable('now', new \DateTimeZone('UTC')));
-    $core = $twig->getExtension(CoreExtension::class);
-    $core->setTimezone('UTC');
-    $core->setDateFormat('Y-m-d H:i e', '%d days');
+$twig->addGlobal('now', new \DateTimeImmutable('now', new \DateTimeZone('UTC')));
+$twig->addGlobal('eternium_url', 'https://www.eterniumgame.com/');
+$twig->addGlobal('pythia_url', 'http://pythia.42web.io/');
+$twig->addGlobal('events', $this->events);
 
-    return $twig;
-})();
+$core = $twig->getExtension(CoreExtension::class);
+$core->setTimezone('UTC');
+$core->setDateFormat('Y-m-d H:i e', '%d days');
+
+return $twig;
