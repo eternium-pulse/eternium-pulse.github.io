e916efbd6b90082cd58449eac488ba98cdfe6d89
Alex Tsarkov <alex.tsarkov@gmail.com>
2024-01-07 17:53:30 +0300

    fix(config): use canonical names

--- src/Config.php
+++ src/Config.php
@@ -7,20 +7,20 @@ class Config
 {
     public function __construct(private string $path)
     {
-        assert(is_dir($path));
+        \assert(\is_dir($path));
 
-        $this->path = realpath($path);
+        $this->path = \realpath($path);
     }
 
     public function __get(string $name)
     {
-        assert('' !== $name);
+        \assert('' !== $name);
 
         $this->{$name} = null;
         $file = "{$this->path}/{$name}.php";
-        if (is_file($file)) {
+        if (\is_file($file)) {
             $this->{$name} = (function () {
-                return require func_get_arg(0);
+                return require \func_get_arg(0);
             })($file);
         }
 
